c863a0c362b4693032814af81ba348d657149734
Robert Enzmann <robert.enzmann@antuit.com>
2021-06-13 00:06:22 -0400

    add linking script

---
+++ link.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+ln -nfs ~/repos/qmk_keymaps/planck_grid/keymap.c ~/repos/qmk_firmware/keyboards/planck/keymaps/renzmann/keymap.c
+ln -nfs ~/repos/qmk_keymaps/planck_grid/rules.mk ~/repos/qmk_firmware/keyboards/planck/keymaps/renzmann/rules.mk
+ln -nfs ~/repos/qmk_keymaps/planck_grid/mykeys.h ~/repos/qmk_firmware/keyboards/planck/keymaps/renzmann/mykeys.h
+ln -nfs ~/repos/qmk_keymaps/config.h ~/repos/qmk_firmware/keyboards/planck/keymaps/renzmann/config.h
+ln -nfs ~/repos/qmk_keymaps/mymacro.h ~/repos/qmk_firmware/keyboards/planck/keymaps/renzmann/mymacro.h
+
